cbb8b0752bfb4808a89c9ad8007eccc77dfb6a48
Javier Barrera <29jbarrera@gmail.com>
2025-03-18 18:46:21 +0100

    add primeng v17

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [
+    InputTextModule, // Importa el módulo de InputText
+    ButtonModule, // Importa el módulo de Button
+    PasswordModule,
+    CommonModule,
+  ],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss'
+  styleUrl: './auth.component.scss',
 })
-export class AuthComponent {
-
-}
+export class AuthComponent {}
