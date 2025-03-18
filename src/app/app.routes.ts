7bf5056bf1ce003b988cb2f63757834000426de8
Javier Barrera <29jbarrera@gmail.com>
2025-03-18 18:08:29 +0100

    add rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,55 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
+import { NavComponent } from './components/nav/nav.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { FacturasComponent } from './pages/facturas/facturas.component';
+import { GastosComponent } from './pages/gastos/gastos.component';
+import { IngresosComponent } from './pages/ingresos/ingresos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent,
+  },
+  {
+    path: 'dashboard',
+    component: NavComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'resumen',
+        pathMatch: 'full',
+      },
+      {
+        path: 'resumen',
+        component: DashboardComponent,
+      },
+      {
+        path: 'admin',
+        component: AdminComponent,
+      },
+      {
+        path: 'clientes',
+        component: ClientesComponent,
+      },
+      {
+        path: 'facturas',
+        component: FacturasComponent,
+      },
+      {
+        path: 'gastos',
+        component: GastosComponent,
+      },
+      {
+        path: 'ingresos',
+        component: IngresosComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '', // Redirecciona rutas no encontradas a auth
+  },
+];
