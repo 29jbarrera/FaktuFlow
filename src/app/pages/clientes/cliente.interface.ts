334130d25e08db560e0c1082cb0d4a66833740df
Javier Barrera <29jbarrera@gmail.com>
2025-03-29 19:34:01 +0100

    create service clientes + interface

---
+++ src/app/pages/clientes/cliente.interface.ts
@@ -0,0 +1,8 @@
+export interface Cliente {
+  id: number;
+  usuario_id: number;
+  telefono: number;
+  nombre: string;
+  email: string;
+  direccion_fiscal: string;
+}
---
+++ src/app/pages/clientes/clientes.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ClientesService {
+  private apiUrl = `${environment.apiUrl}clientes`;
+
+  constructor(private http: HttpClient) {}
+
+  getClientes(): Observable<any> {
+    const token = sessionStorage.getItem('authToken'); // Obtener el token almacenado
+
+    if (!token) {
+      console.error('❌ Error: No hay token en sessionStorage');
+      throw new Error('No hay token disponible');
+    }
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`, // Incluir el token en la cabecera
+    });
+
+    return this.http.get<any>(this.apiUrl, { headers });
+  }
+}
