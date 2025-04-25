5c8098575a4df001857eaacfaa71ee7a1ed4773a
Javier Barrera <29jbarrera@gmail.com>
2025-04-25 18:27:41 +0200

    add politica-cookies

---
+++ src/app/pages/politica-cookies/politica-cookies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../../components/footer/footer.component';
+
+@Component({
+  selector: 'app-politica-cookies',
+  imports: [FooterComponent],
+  templateUrl: './politica-cookies.component.html',
+  styleUrl: './politica-cookies.component.scss',
+})
+export class PoliticaCookiesComponent {}
