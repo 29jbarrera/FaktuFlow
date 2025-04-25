98e43cb20166fe3a5ecbea4741d01d9175a0b1c4
Javier Barrera <29jbarrera@gmail.com>
2025-04-25 18:27:53 +0200

    add politica-privacidad

---
+++ src/app/pages/politica-privacidad/politica-privacidad.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../../components/footer/footer.component';
+
+@Component({
+  selector: 'app-politica-privacidad',
+  imports: [FooterComponent],
+  templateUrl: './politica-privacidad.component.html',
+  styleUrl: './politica-privacidad.component.scss',
+})
+export class PoliticaPrivacidadComponent {}
