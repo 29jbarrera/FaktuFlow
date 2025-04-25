f73b3238b9d306d87d33ef9df91031f99c77716d
Javier Barrera <29jbarrera@gmail.com>
2025-04-25 18:27:27 +0200

    add aviso-legal

---
+++ src/app/pages/aviso-legal/aviso-legal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../../components/footer/footer.component';
+
+@Component({
+  selector: 'app-aviso-legal',
+  imports: [FooterComponent],
+  templateUrl: './aviso-legal.component.html',
+  styleUrl: './aviso-legal.component.scss',
+})
+export class AvisoLegalComponent {}
