0e86c91aa20cec64f7f3e67dae890a9e75121577
Javier Barrera <29jbarrera@gmail.com>
2025-05-18 11:18:11 +0200

    add min-w-nav + change bug pipe

--- src/app/shared/utils/import-eur.pipe.ts
+++ src/app/shared/utils/import-eur.pipe.ts
@@ -4,14 +4,17 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'importeEur',
 })
 export class ImporteEurPipe implements PipeTransform {
-  transform(value: number | null | undefined): string {
-    if (typeof value === 'number') {
-      const formatted = value.toLocaleString('es-ES', {
+  transform(value: number | string | null | undefined): string {
+    const num = typeof value === 'number' ? value : parseFloat(value ?? '');
+
+    if (!isNaN(num)) {
+      const formatted = num.toLocaleString('es-ES', {
         minimumFractionDigits: 2,
         maximumFractionDigits: 2,
       });
       return `${formatted} €`;
     }
+
     return '';
   }
 }
