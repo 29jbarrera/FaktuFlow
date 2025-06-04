ff95f810abbef0c1cfdeb918c267c970b29b86dd
Javier Barrera <29jbarrera@gmail.com>
2025-06-04 10:55:51 +0200

    visual changes blogs add imgs

--- src/app/blogs/productividad/productividad.component.ts
+++ src/app/blogs/productividad/productividad.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBlogsComponent } from '../nav-blogs/nav-blogs.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-productividad',
   standalone: true,
-  imports: [NavBlogsComponent],
+  imports: [NavBlogsComponent, FooterComponent],
   templateUrl: './productividad.component.html',
   styleUrl: './productividad.component.scss',
 })
